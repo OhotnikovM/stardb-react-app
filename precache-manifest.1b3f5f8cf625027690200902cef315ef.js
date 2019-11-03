afac019cff48f95f216eb4e9cdbe409639c1d681
Михаил Охотников <mihailohotnikov@MacBook-Pro-Mihail.local>
2019-11-03 20:10:07 +0300

    Updates

---
+++ precache-manifest.1b3f5f8cf625027690200902cef315ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97d88a4ea7cb0eb658db6f11b0be0d2",
+    "url": "/stardb-react-app/index.html"
+  },
+  {
+    "revision": "68b9cacc8202b6a84555",
+    "url": "/stardb-react-app/static/css/main.7499e78a.chunk.css"
+  },
+  {
+    "revision": "6a61585ff8fd709f1482",
+    "url": "/stardb-react-app/static/js/2.e68e8170.chunk.js"
+  },
+  {
+    "revision": "68b9cacc8202b6a84555",
+    "url": "/stardb-react-app/static/js/main.5270f1b2.chunk.js"
+  },
+  {
+    "revision": "486e1c7b50f4d309b3d6",
+    "url": "/stardb-react-app/static/js/runtime~main.c79d4c8f.js"
+  },
+  {
+    "revision": "5733d3562ec8327c6c4338952801faad",
+    "url": "/stardb-react-app/static/media/deathStar.5733d356.png"
+  }
+]);
